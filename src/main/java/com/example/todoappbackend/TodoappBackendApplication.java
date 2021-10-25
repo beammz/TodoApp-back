0dedef58be7e0eb4cb86c411447a812927acf14c
Beatriz <beammz@gmail.com>
2021-10-25 16:06:07 -0300

    initial commit

---
+++ src/main/java/com/example/todoappbackend/TodoappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappBackendApplication.class, args);
+	}
+
+}
