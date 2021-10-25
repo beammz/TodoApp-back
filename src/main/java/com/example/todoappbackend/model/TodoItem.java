3eda4903ea5ae807974765f3b16f67e51da6797a
Beatriz <beammz@gmail.com>
2021-10-25 16:19:12 -0300

    adding constructor to class

--- src/main/java/com/example/todoappbackend/model/TodoItem.java
+++ src/main/java/com/example/todoappbackend/model/TodoItem.java
@@ -1,14 +1,24 @@
 package com.example.todoappbackend.model;
 
+import javax.persistence.Entity;
+
 /*
 	Author: Beatriz Medeiros Mazieri
 	This class basically holds entire data of that particularly to-do item
 */
+
+@Entity
 public class TodoItem {
 
 	private Long id;
 	private String title;
 	private boolean done;
+	
+	public TodoItem(Long id, String title, boolean done) {
+		this.id = id;
+		this.title = title;
+		this.done = done;
+	}	
 	
 	
 	
