fde585cefbaa7c06de2bea57017e5053820aab03
Beatriz <beammz@gmail.com>
2021-10-25 16:13:28 -0300

    creating class TodoItem

---
+++ src/main/java/com/example/todoappbackend/model/TodoItem.java
@@ -0,0 +1,15 @@
+package com.example.todoappbackend.model;
+
+/*
+	Author: Beatriz Medeiros Mazieri
+	This class basically holds entire data of that particularly to-do item
+*/
+public class TodoItem {
+
+	private Long id;
+	private String title;
+	private boolean done;
+	
+	
+	
+}
